7c64c76e4e16e67b3d4c86e1698a395791c21f0b
sulenn <273409891@qq.com>
2019-11-12 12:36:52 +0800

    add 'Container With Most Water' algorithm

---
+++ array/double point algorithm/Container With Most Water.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func maxArea(height []int) int {
+	frontPoint := 0
+	backPoint := len(height) - 1
+	maxArea := 0
+	for frontPoint < backPoint {
+		if height[frontPoint] > height[backPoint] {
+			tempArea := height[backPoint] * (backPoint - frontPoint)
+			if tempArea > maxArea {
+				maxArea = tempArea
+			}
+			backPoint --
+		} else {
+			tempArea := height[frontPoint] * (backPoint - frontPoint)
+			if tempArea > maxArea {
+				maxArea = tempArea
+			}
+			frontPoint++
+		}
+	}
+	return maxArea
+}
+
+func main() {
+	fmt.Println(maxArea([]int {1,8,6,2,5,4,8,3,7}))
+}
