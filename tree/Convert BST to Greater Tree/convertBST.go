12767826f8861d6c4ae4a66afdb883bcca69343e
sulenn <273409891@qq.com>
2019-12-29 15:52:17 +0800

    add and solve 'convert BST to Greater Tree' algorithm

---
+++ tree/Convert BST to Greater Tree/convertBST.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+//https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+var sum int
+
+func convertBST(root *TreeNode) *TreeNode {
+	sum = 0
+	recursive(root)
+	return root
+}
+
+func recursive(root *TreeNode) {
+	if root != nil {
+		recursive(root.Right)
+		sum += root.Val
+		root.Val = sum
+		recursive(root.Left)
+	}
+}
+
+func main() {
+	temp := &TreeNode{5,&TreeNode{2,nil,nil}, &TreeNode{13,nil,nil}}
+	fmt.Println(convertBST(temp))
+}
