c0a5fd1a1fb57fc96825cd56deb4b8c94550f3f9
sulenn <273409891@qq.com>
2019-12-31 17:39:04 +0800

    add and aolve 'Kth Smallest Element in a BST

---
+++ tree/Kth Smallest Element in a BST/kthSmallest.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"../../common"
+	"fmt"
+)
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func kthSmallest(root *common.TreeNode, k int) int {
+	var smallestElement func(root *common.TreeNode)
+	result := 0
+	smallestElement = func(root *common.TreeNode) {
+		if root == nil {
+			return
+		}
+		if k == 0 {   // 找到最小值即可，防止递归遍历所有节点
+			return
+		}
+		smallestElement(root.Left)
+		k--
+		if k == 0{
+			result = root.Val
+		}
+		smallestElement(root.Right)
+	}
+	smallestElement(root)
+	return result
+}
+
+//
+//func smallestElement(root *TreeNode) int {
+//	if root
+//
+//}
+
+func main() {
+	root := common.CreateTree([]int {3,1,4,1000001,2})
+	fmt.Println(kthSmallest(root,1))
+	fmt.Println(kthSmallest(root,2))
+	fmt.Println(kthSmallest(root,4))
+}
