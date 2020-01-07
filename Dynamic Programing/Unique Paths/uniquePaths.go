512f6e9b30abe4631cf58c3e571400ccb28b433a
sulenn <273409891@qq.com>
2020-01-07 11:07:26 +0800

    add and solve 'unique Paths ||' algorithm

---
+++ Dynamic Programing/Unique Paths/uniquePaths.go
@@ -0,0 +1,29 @@
+package main
+
+//https://leetcode-cn.com/problems/unique-paths/
+
+func uniquePaths(m int, n int) int {
+	if m <= 1 || n <= 1 {
+		return 1
+	}
+	matrix := make([][]int, m)
+	for i := range matrix {
+		matrix[i] = make([]int, n)
+	}
+	for i := range matrix {
+		matrix[i][0] = 1
+	}
+	for i:= range matrix[0] {
+		matrix[0][i] = 1
+	}
+	for i := 1; i< m; i++{
+		for j := 1; j < n; j++ {
+			matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+		}
+	}
+	return matrix[m-1][n-1]
+}
+
+func main() {
+	
+}
