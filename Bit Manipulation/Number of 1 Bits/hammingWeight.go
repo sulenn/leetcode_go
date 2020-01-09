cd6b8c0ca88e34cd3362e1cd97832032a632b2de
sulenn <273409891@qq.com>
2020-01-09 08:58:35 +0800

    add and solve 'Number of 1 Bits' algorithm

---
+++ Bit Manipulation/Number of 1 Bits/hammingWeight.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+////循环和位移动
+//func hammingWeight(num uint32) int {
+//	result := 0
+//	var bit uint32 = 1
+//	for i:=0; i<32; i++ {
+//		if num & bit != 0 {
+//			result++
+//		}
+//		bit = bit << 1
+//	}
+//	return result
+//}
+
+//位操作的小技巧
+//在二进制表示中，数字 nn 中最低位的 11 总是对应 n - 1n−1 中的 00 。因此，将 nn 和 n - 1n−1 与运算总是能把 nn 中最低位的 11 变成 00 ，并保持其他位不变。
+func hammingWeight(num uint32) int {
+	result := 0
+	for num != 0 {
+		result++
+		num = num & (num-1)
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(hammingWeight(11))
+}
