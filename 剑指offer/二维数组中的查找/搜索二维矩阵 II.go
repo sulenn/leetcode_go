902f3e5d25ee9c352ee90793a4d5f6dcec043652
sulenn <273409891@qq.com>
2020-02-08 09:49:23 +0800

    add and solve '二维数组中的查找' algorithm

---
+++ 剑指offer/二维数组中的查找/搜索二维矩阵 II.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
+
+//选左上角，往右走和往下走都增大，不能选
+//
+//选右下角，往上走和往左走都减小，不能选
+//
+//选左下角，往右走增大，往上走减小，可选
+//
+//选右上角，往下走增大，往左走减小，可选
+//参考方法4：https://leetcode-cn.com/problems/search-a-2d-matrix-ii/solution/sou-suo-er-wei-ju-zhen-ii-by-leetcode-2/
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	row := len(matrix)
+	col := len(matrix[0])
+	leftDownRow := row - 1
+	leftDownCol := 0
+	for leftDownRow >= 0 && leftDownCol < col {
+		if matrix[leftDownRow][leftDownCol] == target {return true}
+		if matrix[leftDownRow][leftDownCol] > target {
+			leftDownRow--
+		} else {
+				leftDownCol++
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(searchMatrix([][]int{{1,   4,  7, 11, 15},{2,   5,  8, 12, 19},{3,   6,  9, 16, 22},{10, 13, 14, 17, 24},{18, 21, 23, 26, 30}},5))
+	fmt.Println(searchMatrix([][]int{{1,   4,  7, 11, 15},{2,   5,  8, 12, 19},{3,   6,  9, 16, 22},{10, 13, 14, 17, 24},{18, 21, 23, 26, 30}},20))
+}
---
+++ 剑指offer/二维数组中的查找/搜索二维矩阵.go
@@ -0,0 +1,40 @@
+package main
+//
+////https://leetcode-cn.com/problems/search-a-2d-matrix/submissions/
+//
+//import (
+//	"fmt"
+//)
+//
+////二分法，参考：https://leetcode-cn.com/problems/search-a-2d-matrix/solution/sou-suo-er-wei-ju-zhen-by-leetcode/
+//func searchMatrix(matrix [][]int, target int) bool {
+//	if len(matrix) == 0 {
+//		return false
+//	}
+//	row := len(matrix)
+//	col := len(matrix[0])
+//	prev := 0
+//	tail := row * col - 1
+//	for tail >= prev {
+//		mid := (prev + tail) / 2
+//		if matrix[mid/col][mid%col] == target {
+//			return true
+//		}
+//		if matrix[mid/col][mid%col] > target {
+//			tail = mid - 1
+//		} else {
+//			prev = mid + 1
+//		}
+//	}
+//	return false
+//}
+//
+//func main() {
+//	//test := make([]int,3)
+//	//for i:=0;i<len(test);i++ {
+//	//	test[]
+//	//}
+//	fmt.Println(searchMatrix([][]int{{1,   3,  5,  7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 3))
+//	fmt.Println(searchMatrix([][]int{{1,   3,  5,  7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 13))
+//	fmt.Println(searchMatrix([][]int{{1,   3}}, 3))
+//}
