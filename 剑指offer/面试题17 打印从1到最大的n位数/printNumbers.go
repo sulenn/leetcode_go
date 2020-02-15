55c529433e6928ec69a4356744bb75e451387d75
sulenn <273409891@qq.com>
2020-02-15 17:47:00 +0800

    add 面试题17 打印从1到最大的n位数'

---
+++ 剑指offer/面试题17 打印从1到最大的n位数/printNumbers.go
@@ -0,0 +1,15 @@
+package main
+
+import "math"
+
+//https://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/
+
+//该解法没有考虑到大数的情况，不是 “剑指 offer” 想表达的意思
+func printNumbers(n int) []int {
+	maxNumer := int(math.Pow10(n) - 1)
+	result := make([]int, maxNumer)
+	for i:=0;i<maxNumer;i++ {
+		result[i] = i+1
+	}
+	return result
+}
