9e34e8269e02da2d60ba4418d74398a86ea569f2
sulenn <273409891@qq.com>
2020-02-15 17:13:49 +0800

    add and solve 'est.go.

---
+++ 剑指offer/面试题16 数值的整数次方/myPow_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_myPow(T *testing.T)  {
+	fmt.Println(myPow(2.0, 10))
+	fmt.Println(myPow(2.1, 3))
+	fmt.Println(myPow(2.0, -2))
+	fmt.Println(myPow(0.0, -2))
+	fmt.Println(myPow(0.0, 2))
+	fmt.Println(myPow(0.0, 0))
+	fmt.Println(myPow(0.00000001, -2))
+}
