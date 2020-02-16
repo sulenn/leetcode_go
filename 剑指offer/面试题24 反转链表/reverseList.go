6affede2170b2dab6a1dee8914e182825da9ab76
sulenn <273409891@qq.com>
2020-02-16 16:12:36 +0800

    add and solve '面试题24 反转链表' algorithm

---
+++ 剑指offer/面试题24 反转链表/reverseList.go
@@ -0,0 +1,18 @@
+package main
+
+//https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode = nil
+	for head != nil {
+		curNode := head
+		head, curNode.Next = head.Next, prev
+		prev = curNode
+	}
+	return prev
+}
