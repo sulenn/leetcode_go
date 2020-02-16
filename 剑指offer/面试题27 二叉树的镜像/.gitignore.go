5cde12648688aac691f16adfb17c1139786bfdd5
sulenn <273409891@qq.com>
2020-02-16 19:42:51 +0800

    add and solve ’面试题27 二叉树的镜像' algorithm

---
+++ 剑指offer/面试题27 二叉树的镜像/.gitignore.go
@@ -0,0 +1,19 @@
+package main
+
+//https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/submissions/
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	mirrorTree(root.Left)
+	mirrorTree(root.Right)
+	return root
+}
