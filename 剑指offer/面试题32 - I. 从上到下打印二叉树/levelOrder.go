94c79d3d28d1e863cd439febe5332905eaa10c69
sulenn <273409891@qq.com>
2020-02-16 22:17:14 +0800

    add and solve '面试题32 - I. 从上到下打印二叉树' algorithm

---
+++ 剑指offer/面试题32 - I. 从上到下打印二叉树/levelOrder.go
@@ -0,0 +1,28 @@
+package main
+
+//https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof/
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) []int {
+	if root == nil {
+		return []int {}
+	}
+	queue := []*TreeNode {root}
+	var result []int
+	for len(queue) != 0 {
+		result = append(result, queue[0].Val)
+		if queue[0].Left != nil {
+			queue = append(queue, queue[0].Left)
+		}
+		if queue[0].Right != nil {
+			queue = append(queue, queue[0].Right)
+		}
+		queue = queue[1:]
+	}
+	return result
+}
