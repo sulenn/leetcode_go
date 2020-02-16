edba0b7305d8a68233c930fb247a5658e6e823bb
sulenn <273409891@qq.com>
2020-02-16 11:56:47 +0800

    add and solve 面试题21 调整数组顺序使奇数位于偶数前面' algorithm

---
+++ 剑指offer/面试题21 调整数组顺序使奇数位于偶数前面/exchange.go
@@ -0,0 +1,22 @@
+package main
+
+//https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+
+func exchange(nums []int) []int {
+	head := 0
+	tail := len(nums) - 1
+	for head < tail {
+		if nums[head] & 1 == 0 && nums[tail] & 1 == 1 {  // 头指针指向的元素为偶数；尾指针指向的元素为奇数
+			nums[head], nums[tail] = nums[tail], nums[head]
+			head++
+			tail--
+		}
+		if nums[head] & 1 == 1 {  // 头指针指向的元素为奇数
+			head++
+		}
+		if nums[tail] & 1 == 0 {  // 尾指针指向的元素为偶数
+			tail--
+		}
+	}
+	return nums
+}
---
+++ 剑指offer/面试题21 调整数组顺序使奇数位于偶数前面/exchange_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_exchange(T *testing.T)  {
+	fmt.Println(exchange([]int {1,2,3,4}))
+}
