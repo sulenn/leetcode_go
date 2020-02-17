1fb11baa9bc7c107ad2656dc6db12e3b6db66d0c
sulenn <273409891@qq.com>
2020-02-17 10:33:44 +0800

    add and solve '面试题34. 二叉树中和为某一值的路径' algorithm

---
+++ 剑指offer/面试题34. 二叉树中和为某一值的路径/pathSum.go
@@ -0,0 +1,35 @@
+package main
+
+//https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func pathSum(root *TreeNode, sum int) [][]int {
+	//if root == nil || root.Val > sum {return [][]int {}}
+	if root == nil {return [][]int {}}
+	if root.Left == nil && root.Right == nil && sum == root.Val {  // 满足条件的路径
+		return [][]int {{root.Val}}
+	}
+	if root.Left == nil && root.Right == nil && sum != root.Val {
+		return [][]int {}
+	}
+	leftTreePath := [][]int {}  // 左树路径
+	rightTreePath := [][]int {}  // 右树路径
+	if root.Left != nil {
+		leftTreePath = pathSum(root.Left, sum-root.Val)
+	}
+	for i:=0;i<len(leftTreePath);i++ {
+		leftTreePath[i] = append([]int {root.Val}, leftTreePath[i]...)
+	}
+	if root.Right != nil {
+		rightTreePath = pathSum(root.Right, sum-root.Val)
+	}
+	for i:=0;i<len(rightTreePath);i++ {
+		rightTreePath[i] = append([]int {root.Val}, rightTreePath[i]...)
+	}
+	return append(leftTreePath, rightTreePath...)
+}
