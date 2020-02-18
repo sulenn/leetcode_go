a12e47c1378d8098c93d729e271a31d721a81c14
sulenn <273409891@qq.com>
2020-02-18 20:50:56 +0800

    add and solve '

---
+++ 剑指offer/面试题12. 矩阵中的路径/exist_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_exist(T *testing.T)  {
+	//fmt.Println(exist([][]byte {{'a','b','c','e'},{'s','f','c','s'},{'a','d','e','e'}}, "bfce"))
+	//fmt.Println(exist([][]byte {{'a','b','c','e'},{'s','f','c','s'},{'a','d','e','e'}}, "abfb"))
+	fmt.Println(exist([][]byte {{'a', 'a'}}, "aa"))
+}
