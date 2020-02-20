fe344eeb5ab394600c2c899de0b44c56c6f85033
sulenn <273409891@qq.com>
2020-02-21 00:30:12 +0800

    add and solve '面试13 机器人的运动范围' algorithm

---
+++ 剑指offer/面试题13. 机器人的运动范围/movingCount_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_movingCount(T *testing.T) {
+	//fmt.Println(movingCount(2,3,1))
+	//fmt.Println(movingCount(3,1,0))
+	fmt.Println(movingCount(38,15,9))
+}
