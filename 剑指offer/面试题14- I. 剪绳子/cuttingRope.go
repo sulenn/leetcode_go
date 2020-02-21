b00a2a5d4a9b4276365bb8a87899742a9a8e101a
sulenn <273409891@qq.com>
2020-02-21 09:32:16 +0800

    add and solve '面试题14-I 剪绳子' algorithm

---
+++ 剑指offer/面试题14- I. 剪绳子/cuttingRope.go
@@ -0,0 +1,19 @@
+package main
+
+import "math"
+
+//https://leetcode-cn.com/problems/jian-sheng-zi-lcof/
+
+//**参考**：https://leetcode-cn.com/problems/integer-break/solution/343-zheng-shu-chai-fen-tan-xin-by-jyd/
+//贪心
+func cuttingRope(n int) int {
+	if n <= 3 {return n-1}
+	a := n / 3
+	b := n % 3
+	if b == 1 {
+		b = 4
+		a--
+	}
+	if b == 0 {b=1}
+	return int(math.Pow(float64(3), float64(a)))*b
+}
---
+++ 剑指offer/面试题14- I. 剪绳子/cuttingRope_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_cuttingRope(T *testing.T)  {
+	fmt.Println(cuttingRope(2))
+	fmt.Println(cuttingRope(3))
+	fmt.Println(cuttingRope(10))
+	fmt.Println(cuttingRope(-1))
+}
