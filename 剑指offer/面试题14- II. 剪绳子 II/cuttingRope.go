c2362c4e044893ef3bb993fb2b6e285d70bb03cc
sulenn <273409891@qq.com>
2020-02-21 09:42:03 +0800

    add and solve '面试题14-II 剪绳子 II' algorithm

---
+++ 剑指offer/面试题14- II. 剪绳子 II/cuttingRope.go
@@ -0,0 +1,30 @@
+package main
+
+//https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof/
+
+func cuttingRope(n int) int {
+	if n <= 3 {return n-1}
+	a := n / 3
+	b := n % 3
+	if b == 1 {
+		b = 4
+		a--
+	}
+	if b == 0 {b=1}
+	return mul(a, b)
+}
+
+func mul(a int, b int) int {
+	result := 1
+	for i:=0; i<a; i++ {
+		result *= 3
+		if result > 1000000007 {
+			result %= 1000000007
+		}
+	}
+	result *= b
+	if result > 1000000007 {
+		result %= 1000000007
+	}
+	return result
+}
---
+++ 剑指offer/面试题14- II. 剪绳子 II/cuttingRope_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_cuttingRope(T *testing.T)  {
+	fmt.Println(cuttingRope(120))
+}
