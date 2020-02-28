f91b00a39fab0daa9cc91214c5de6cdfa59ea4eb
sulenn <273409891@qq.com>
2020-02-28 16:13:40 +0800

    enheng

--- leetcode/sql/176. 第二高的薪水/第二高的薪水.sql
+++ leetcode/sql/176. 第二高的薪水/第二高的薪水.sql
@@ -1,5 +1,5 @@
 -- https://leetcode-cn.com/problems/second-highest-salary/submissions/
-SELECT (SELECT Salary
+SELECT (SELECT DISTINCT  Salary
 FROM Employee
 ORDER BY Salary DESC
 LIMIT 1, 1) AS SecondHighestSalary;
