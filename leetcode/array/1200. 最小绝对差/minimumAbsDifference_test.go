be9089f36b8ca40587c5e7e9dfd86c94a3fe9fb0
sulenn <273409891@qq.com>
2020-03-03 11:30:47 +0800

    add and solve '1200. 最小绝对差' algorithm

---
+++ leetcode/array/1200. 最小绝对差/minimumAbsDifference_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_minimumAbsDifference(T *testing.T)  {
+	fmt.Println(minimumAbsDifference([]int {4,2,1,3}))
+	fmt.Println(minimumAbsDifference([]int {1,3,6,10,15}))
+	fmt.Println(minimumAbsDifference([]int {3,8,-10,23,19,-4,-14,27}))
+}
