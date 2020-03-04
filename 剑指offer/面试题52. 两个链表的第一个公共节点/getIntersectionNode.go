6fb36e9ec1264d29f67fb54eca922339ee140f64
sulenn <273409891@qq.com>
2020-03-04 17:58:14 +0800

    Add and solve '面试题52 两个链表的第一个公共节点' algorithm

---
+++ 剑指offer/面试题52. 两个链表的第一个公共节点/getIntersectionNode.go
@@ -0,0 +1,27 @@
+package 面试题52__两个链表的第一个公共节点
+
+//https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	newHeadA := headA
+	newHeadB := headB
+
+	for newHeadA != newHeadB {
+		if newHeadA == nil{
+			newHeadA = headB
+		} else {
+			newHeadA = newHeadA.Next
+		}
+		if newHeadB == nil {
+			newHeadB = headA
+		} else {
+			newHeadB = newHeadB.Next
+		}
+	}
+	return newHeadA
+}
