818874c2395665500329b5b15d6ead0d43533646
sulenn <273409891@qq.com>
2020-03-05 00:15:46 +0800

    Add and solve '面试题56-| 数组中数字出现的次数' algorithm:

---
+++ 剑指offer/面试题56 - I. 数组中数字出现的次数/singleNumbers_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_singleNumbers(T *testing.T) {
+	//fmt.Println(singleNumbers([]int {4,1,4,6}))
+	fmt.Println(singleNumbers([]int {3,2,1,3}))
+}
