e7f3805a23640cc6c9d272eef9add4ff0a2bf62a
sulenn <273409891@qq.com>
2020-03-04 15:42:57 +0800

    add and solve '第一个只出现一次的字符’ algorithm

---
+++ 剑指offer/面试题50. 第一个只出现一次的字符/firstUniqChar.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
+
+func firstUniqChar(s string) byte {
+	sMap := make(map[byte]int)
+	for i:=0; i< len(s); i++ {
+		if _, ok := sMap[s[i]]; ok {
+			sMap[s[i]]++
+		} else {
+			sMap[s[i]] = 1
+		}
+	}
+	for i:=0; i< len(s); i++ {
+		if sMap[s[i]] == 1 {
+			return s[i]
+		}
+	}
+	return ' '
+}
+
+func main()  {
+	test := make(map[int]int)
+	fmt.Println(test[0])
+}
---
+++ 剑指offer/面试题50. 第一个只出现一次的字符/firstUniqChar_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_firstUniqChar(T *testing.T) {
+	fmt.Println(firstUniqChar("firstUniqChar"))
+}
