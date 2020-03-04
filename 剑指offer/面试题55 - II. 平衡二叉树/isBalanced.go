a04d5d5c9c79d17271d212bd55346e13d864ed13
sulenn <273409891@qq.com>
2020-03-04 23:18:23 +0800

    Add and solve '面试题55-|| 平衡二叉树' algorithm

---
+++ 剑指offer/面试题55 - II. 平衡二叉树/isBalanced.go
@@ -0,0 +1,26 @@
+package main
+
+//https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	var preOrder func(root *TreeNode) int
+	preOrder = func(root *TreeNode) int {
+		if root == nil {return 0}
+		left := preOrder(root.Left)
+		right := preOrder(root.Right)
+		if left == -1 || right == -1 || left - right > 1 || left - right < -1 {return -1}
+		if left > right {
+			return left + 1
+		} else {
+			return right + 1
+		}
+	}
+	return preOrder(root) != -1
+}
+
