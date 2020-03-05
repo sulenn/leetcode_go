d5a3c6d8ce5c9914235026efaf3d89a9ab8c101e
sulenn <273409891@qq.com>
2020-03-05 10:24:40 +0800

    add and solve '面试57 - || 和为s的连续正数序列' algorithm

---
+++ 剑指offer/面试题57 - II. 和为s的连续正数序列/findContinuousSequence.go
@@ -0,0 +1,26 @@
+package main
+
+//https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/
+
+// 双指针
+func findContinuousSequence(target int) [][]int {
+	result := [][]int {}
+	start := 1
+	end := 2
+	for start < end {
+		if (start + end)*(end-start+1)/2 == target {   // 等差求和
+			subResult := []int {}
+			for i:= start; i<= end; i++ {   // 满足条件的连续和
+				subResult = append(subResult, i)
+			}
+			result = append(result, subResult)
+			start++
+		}
+		if (start + end)*(end-start+1)/2 > target {
+			start++
+		} else {
+				end++
+		}
+	}
+	return result
+}
---
+++ 剑指offer/面试题57 - II. 和为s的连续正数序列/findContinuousSequence_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_findContinuousSequence(T *testing.T) {
+	fmt.Println(findContinuousSequence(9))
+	fmt.Println(findContinuousSequence(15))
+}
