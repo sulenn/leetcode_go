f02c2b8d0f07375ad40abac1c774f457f6249bf6
sulenn <273409891@qq.com>
2020-03-09 15:27:23 +0800

    Add and solve '1089. 复写零' algorithm

---
+++ leetcode/array/1089. 复写零/duplicateZeros_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_duplicateZeros(T *testing.T) {
+	duplicateZeros([]int {1,0,2,3,0,4,5,0})
+	duplicateZeros([]int {1,0,2,3,0,0})
+	duplicateZeros([]int {1,2,3})
+}
