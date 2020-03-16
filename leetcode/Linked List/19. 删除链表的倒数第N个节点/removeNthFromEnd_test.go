1c701a29bd5df821c25c8b8cd93980b590fa3545
sulenn <273409891@qq.com>
2020-03-16 09:41:37 +0800

    add and solve '19. 删除链表的倒数第N个结点' algorithm

---
+++ leetcode/Linked List/19. 删除链表的倒数第N个节点/removeNthFromEnd_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_removeNthFromEnd(T *testing.T) {
+	head := &ListNode{5,nil}
+	print(removeNthFromEnd(head, 1))
+	head.Next = &ListNode{7,nil}
+	print(removeNthFromEnd(head, 2))
+	print(removeNthFromEnd(head, 1))
+	print(removeNthFromEnd(head, 3))
+}
+
+func print(head *ListNode)  {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
