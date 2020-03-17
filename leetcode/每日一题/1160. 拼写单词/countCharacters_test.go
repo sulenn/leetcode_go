f8cf81417dd6501750173410d2db00ded3f108c8
sulenn <273409891@qq.com>
2020-03-17 08:37:56 +0800

    add and solve '拼写单词' algorithm

---
+++ leetcode/每日一题/1160. 拼写单词/countCharacters_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_countCharacters(T *testing.T) {
+	fmt.Println(countCharacters([]string {"cat","bt","hat","tree"}, "atach"))
+	fmt.Println(countCharacters([]string {"hello","world","leetcode"}, "welldonehoneyr"))
+	fmt.Println(countCharacters([]string {"hello","world","leetcode"}, ""))
+	fmt.Println(countCharacters([]string {}, "welldonehoneyr"))
+}
