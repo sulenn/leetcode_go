ddd1f2ff59a3f4e755115a51f4b8b5221f011b68
sulenn <273409891@qq.com>
2020-03-19 08:52:55 +0800

    add and solve '409. 最长回文串' algorithm

---
+++ leetcode/每日一题/409. 最长回文串/longestPalindrome.go
@@ -0,0 +1,23 @@
+package main
+
+//https://leetcode-cn.com/problems/longest-palindrome/
+
+func longestPalindrome(s string) int {
+	alpha := make([]int, 52)
+	for i:=0; i<len(s); i++ {
+		if s[i] >= 'A' && s[i] <= 'Z' {   // 大写范围 0-25
+			alpha[s[i]-'A']++
+		} else {  // 小写范围 26 - 51
+			alpha[s[i]-'a'+26]++
+		}
+	}
+	result := 0
+	for _,v:= range alpha {
+		result += v/2*2
+	}
+	if result != len(s) {  // 存在奇数
+		return result+1
+	} else {  // 不存在奇数
+		return result
+	}
+}
---
+++ leetcode/每日一题/409. 最长回文串/longestPalindrome_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_longestPalindrome(T *testing.T) {
+	//fmt.Println(longestPalindrome("abccccdd"))
+	fmt.Println(longestPalindrome("ccc"))
+}
