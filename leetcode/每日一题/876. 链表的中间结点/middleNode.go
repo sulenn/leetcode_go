0c5b53be754e2e24349fc7b4d2d88fc86e38445d
sulenn <273409891@qq.com>
2020-03-23 08:09:46 +0800

    add and solve '876. 链表的中间结点' algortihm

---
+++ leetcode/每日一题/876. 链表的中间结点/middleNode.go
@@ -0,0 +1,19 @@
+package main
+
+//https://leetcode-cn.com/problems/middle-of-the-linked-list/
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+func middleNode(head *ListNode) *ListNode {
+	p := head
+	for p.Next != nil && p.Next.Next != nil {
+		head = head.Next
+		p = p.Next.Next
+	}
+	if p.Next != nil {
+		return head.Next
+	}
+	return head
+}
