72e6bb54303157cfa22b886091c97616787c072e
sulenn <273409891@qq.com>
2020-03-27 20:03:21 +0800

    add and solve '744. 寻找比目标字母大的最小字母' algorithm

---
+++ leetcode/twosearch/744. 寻找比目标字母大的最小字母/nextGreatestLetter_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_nextGreatestLetter(T *testing.T) {
+	fmt.Println(nextGreatestLetter([]byte {'c','f','j'},'a'))
+	fmt.Println(nextGreatestLetter([]byte {'j','c','f'},'a'))
+	fmt.Println(nextGreatestLetter([]byte {'j','c','f'},'c'))
+	fmt.Println(nextGreatestLetter([]byte {'j','c','f'},'d'))
+	fmt.Println(nextGreatestLetter([]byte {'j','c','f'},'g'))
+	fmt.Println(nextGreatestLetter([]byte {'j','c','f'},'j'))
+	fmt.Println(nextGreatestLetter([]byte {'j','c','f'},'k'))
+}
