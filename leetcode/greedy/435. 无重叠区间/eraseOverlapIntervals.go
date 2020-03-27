a4d18b8452943a1942ec4ed4b4d6f4e83692bcd5
sulenn <273409891@qq.com>
2020-03-27 09:51:07 +0800

    add and solve '435. 无重叠区间' algorithm

---
+++ leetcode/greedy/435. 无重叠区间/eraseOverlapIntervals.go
@@ -0,0 +1,32 @@
+package main
+
+import "sort"
+
+//https://leetcode-cn.com/problems/non-overlapping-intervals/description/
+
+func eraseOverlapIntervals(intervals [][]int) int {
+	//for i:=0; i<len(intervals); i++ {   // 插入排序，先比较区间末端，如果末端相等，再比较区间头
+	//	for j:=i;j>0;j-- {
+	//		if intervals[j][1] < intervals[j-1][1] || (intervals[j][1] == intervals[j-1][1] && intervals[j-1][0] < intervals[j][0]) {
+	//			intervals[j], intervals[j-1] = intervals[j-1], intervals[j]
+	//		}
+	//	}
+	//}
+	sort.Slice(intervals, func(i, j int) bool {  // 排序，只考虑区间末端即可
+		return intervals[i][1] < intervals[j][1]
+	})
+	count := 0
+	cur := []int {}   // 最近的区间
+	for j:=0; j<len(intervals); j++ {
+		if j == 0 {
+			count++
+			cur = intervals[0]
+			continue
+		}
+		if intervals[j][0]>=cur[1] {
+			cur = intervals[j]
+			count++
+		}
+	}
+	return len(intervals) - count
+}
---
+++ leetcode/greedy/435. 无重叠区间/eraseOverlapIntervals_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_eraseOverlapIntervals(T *testing.T) {
+	fmt.Println(eraseOverlapIntervals([][]int {{1,2},{2,3},{3,4},{1,3}}))
+	fmt.Println(eraseOverlapIntervals([][]int {{1,2},{1,2},{1,2}}))
+	fmt.Println(eraseOverlapIntervals([][]int {{1,2},{2,3}}))
+	fmt.Println(eraseOverlapIntervals([][]int {}))
+	fmt.Println(eraseOverlapIntervals([][]int {{1,100},{11,22},{1,11},{2,12}}))
+}
