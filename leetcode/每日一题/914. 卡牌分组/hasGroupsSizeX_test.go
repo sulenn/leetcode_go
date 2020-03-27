2f668f02db4ec072ccbe91e3f3ac62153df75451
sulenn <273409891@qq.com>
2020-03-27 08:47:34 +0800

    Add and solve '914. 卡牌分组' algorithm

---
+++ leetcode/每日一题/914. 卡牌分组/hasGroupsSizeX_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_hasGroupsSizeX(T *testing.T) {
+	fmt.Println(hasGroupsSizeX([]int {1,2,3,4,4,3,2,1}))
+	fmt.Println(hasGroupsSizeX([]int {1,1,1,2,2,2,3,3}))
+	fmt.Println(hasGroupsSizeX([]int {1}))
+	fmt.Println(hasGroupsSizeX([]int {1,1}))
+	fmt.Println(hasGroupsSizeX([]int {1,1,2,2,2,2}))
+	fmt.Println(hasGroupsSizeX([]int {}))
+}
