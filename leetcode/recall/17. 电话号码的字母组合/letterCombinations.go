78981fe0df79530c421d6d40d62771de6d9ee0b2
sulenn <273409891@qq.com>
2020-03-28 10:09:51 +0800

    add and solve '17. 电话号码的字母组合' algorithm

---
+++ leetcode/recall/17. 电话号码的字母组合/letterCombinations.go
@@ -0,0 +1,23 @@
+package main
+
+//https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+
+func letterCombinations(digits string) []string {
+	var dfs func(digits string, str string)
+	dic := map[byte]string {'2':"abc",'3':"def",'4':"ghi",'5':"jkl",'6':"mno",'7':"pqrs",'8':"tuv",'9':"wxyz"}
+	result := make([]string,0)
+	dfs = func(digits string, str string) {
+		if len(digits) == 0 {
+			if str != "" {
+				result = append(result, str)
+			}
+			return
+		}
+		nums := dic[digits[0]]
+		for i:=0; i<len(nums); i++ {
+			dfs(digits[1:], str+string(nums[i]))
+		}
+	}
+	dfs(digits, "")
+	return result
+}
---
+++ leetcode/recall/17. 电话号码的字母组合/letterCombinations_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_letterCombinations(T *testing.T) {
+	//fmt.Println(letterCombinations("23"))
+	fmt.Println(letterCombinations(""))
+}
