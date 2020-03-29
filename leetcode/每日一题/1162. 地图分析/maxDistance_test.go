27ab75c8674ffb3c16f4a7269026ccafe31f23c5
sulenn <273409891@qq.com>
2020-03-29 09:25:49 +0800

    add and solve '1162. 地图分析' algorithm

---
+++ leetcode/每日一题/1162. 地图分析/maxDistance_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_maxDistance(T *testing.T) {
+	fmt.Println(maxDistance([][]int {{1,0,1},{0,0,0},{1,0,1}}))
+	//fmt.Println(maxDistance([][]int {{1,0,0},{0,0,0},{0,0,0}}))
+}
