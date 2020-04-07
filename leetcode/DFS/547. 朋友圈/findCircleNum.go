0fdb9290ffb98fb11f5072f883f6f3cf269ca5f1
sulenn <273409891@qq.com>
2020-04-07 17:21:22 +0800

    add and solve '547. 朋友圈' algorithm

---
+++ leetcode/DFS/547. 朋友圈/findCircleNum.go
@@ -0,0 +1,26 @@
+package main
+
+//https://leetcode-cn.com/problems/friend-circles/
+
+// 深搜
+func findCircleNum(M [][]int) int {
+	visited := make([]bool, len(M))
+	count := 0
+	for i:=0; i<len(M); i++ {
+		if !visited[i] {
+			count++
+			visited[i]=true
+			dfs(M, visited, i)
+		}
+	}
+	return count
+}
+
+func dfs(M [][]int, visited []bool, num int)  {
+	for i:=0; i<len(M); i++ {
+		if M[num][i] == 1 && !visited[i] {
+			visited[i] = true
+			dfs(M,visited, i)
+		}
+	}
+}
