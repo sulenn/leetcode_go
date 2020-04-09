c99cfd23f44d1cef54d574bc16749ab6efc60bf2
sulenn <273409891@qq.com>
2020-04-09 09:15:27 +0800

    add and solve '22. 括号生成' algorithm

---
+++ leetcode/每日一题/22. 括号生成/generateParenthesis.go
@@ -0,0 +1,23 @@
+package main
+
+//https://leetcode-cn.com/problems/generate-parentheses/
+
+//深搜
+func generateParenthesis(n int) []string {
+	if n == 0 {return []string {}}
+	var dfs func(l,r int, str string)
+	result := []string {}
+	dfs = func(l, r int, str string) {
+		if l == 0 && r == 0 {
+			result = append(result, str)
+		}
+		if l > 0 {
+			dfs(l-1, r, str+"(")
+		}
+		if r > l {
+			dfs(l, r-1, str+")")
+		}
+	}
+	dfs(n,n,"")
+	return result
+}
---
+++ leetcode/每日一题/22. 括号生成/generateParenthesis_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_generateParenthesis(T *testing.T) {
+	fmt.Println(generateParenthesis(3))
+}
