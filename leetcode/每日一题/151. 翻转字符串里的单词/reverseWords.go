a9b95a0e6342eed99405815f544299a7a805953f
sulenn <273409891@qq.com>
2020-04-10 08:08:51 +0800

    add and solve '151. 反转字符串里的单词' algorithm

---
+++ leetcode/每日一题/151. 翻转字符串里的单词/reverseWords.go
@@ -0,0 +1,25 @@
+package main
+
+//https://leetcode-cn.com/problems/reverse-words-in-a-string/
+
+import (
+	"fmt"
+	"strings"
+)
+
+func reverseWords(s string) string {
+	sArr := strings.Split(s, " ")
+	newArr := make([]string, 0)
+	for i:=len(sArr)-1; i>=0; i-- {
+		if sArr[i] != "" {
+			newArr = append(newArr, sArr[i])
+		}
+	}
+	return strings.Join(newArr," ")
+}
+
+func main() {
+	fmt.Println(reverseWords("the sky is blue"))
+	fmt.Println(reverseWords("a good   example"))
+	fmt.Println(reverseWords("  a good   example  "))
+}
