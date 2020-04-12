da8e29f5e919c270953bd5bc9cddccf06f45391b
sulenn <273409891@qq.com>
2020-04-12 11:52:41 +0800

    add and solve '974. 和可被K整除的子数组' algorithm

---
+++ leetcode/array/974. 和可被 K 整除的子数组/subarraysDivByK.go
@@ -0,0 +1,19 @@
+package main
+
+//https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/
+
+func subarraysDivByK(A []int, K int) int {
+	sum := 0
+	dic := make(map[int]int)
+	dic[0] = 1    // 字典初始化
+	count := 0
+	for i:=0; i<len(A); i++ {
+		sum += A[i]
+		sum = (sum % K + K) % K    // 注意这儿需要考虑负数，所以 + K
+		if _, ok := dic[sum]; ok {
+			count += dic[sum]
+		}
+		dic[sum]++
+	}
+	return count
+}
