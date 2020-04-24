de6e539d2335985046d9a210637d15aedbdf22ac
sulenn <273409891@qq.com>
2020-04-24 08:32:07 +0800

    add and solve 两两交换链表中的节点 algorithm

---
+++ leetcode/Linked List/24. 两两交换链表中的节点/swapPairs.go
@@ -0,0 +1,32 @@
+package main
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+// 每一次交换，p2.next = p3.next, p3.next = p2, p1.next
+// 需要注意判断临界值
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil {return nil}
+	var p1 *ListNode = nil
+	p2 := head
+	p3 := head.Next
+	head = p3
+	for p2 != nil && p3 != nil {
+		p2.Next = p3.Next
+		p3.Next = p2
+		if p1 != nil {  // 注意刚开始，p1 为 nil
+			p1.Next = p3
+		}
+		p1 = p2
+		p2 = p2.Next
+		if p2 != nil {  // 小心越界
+			p3 = p2.Next
+		} else {
+			p3 = nil
+		}
+	}
+	if head != nil {return head} // 链表中只有一个元素
+	return p2
+}
--- test.go
+++ test.go
@@ -1,44 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"strings"
+import "fmt"
+
+const (
+	Byte FileSize = 1 << (iota * 10)
+	KiByte
+	MiByte
+	GiByte
 )
 
-type name struct {
-	arr []int
-}
 
-func Construc(nums []int) *name {
-	return &name{nums}
-}
-
-func (this *name) add(num int)  {
-	//test := this.arr
-	this.arr = append(this.arr, num)
-	//test = append(test, num)
-}
-
-//func main() {
-//	var i *int
-//	i=new(int)
-//	//*i=10
-//	fmt.Println(*i)
-//
-//}
+type FileSize int64
 
 func main() {
-	//fmt.Println(commonPre([]string {"flower","flow","flight"}))
-	//fmt.Println(commonPre([]string {"dog","racecar","car"}))
-	//fmt.Println(time.Now().Unix())
-	//var a string
-	//fmt.Scanln(&a)
-	//fmt.Println(a)
-	var test []string
-	fmt.Println(test)
-	test = append(test,"2")
-	fmt.Println(test)
-	var test1 = make([]string, 0)
-	fmt.Println(test1)
-	strings.Repeat()
+	fmt.Println(Byte)
+	fmt.Println(KiByte)
 }
