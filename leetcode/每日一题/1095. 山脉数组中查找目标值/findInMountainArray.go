d3d97c63cb13ab743f20dbecb3f50c47b2ccc026
sulenn <273409891@qq.com>
2020-05-01 10:07:39 +0800

    add and solve '1095. 山脉数组中查找目标值' algorithm

--- leetcode/每日一题/1095. 山脉数组中查找目标值/findInMountainArray.go
+++ leetcode/每日一题/1095. 山脉数组中查找目标值/findInMountainArray.go
@@ -12,25 +12,47 @@ package main
 
 func findInMountainArray(target int, mountainArr *MountainArray) int {
 	length := mountainArr.length()
-	h := 0
-	t := length-1
-	mid := h + (t-h)/2
-	midValue := mountainArr.get(mid)
-	if midValue == target {
-		return mid
-	} else if midValue < target {
-		if mid-1 >= 0 &&
-	}
+	mountainIndex := findMountainLocation(mountainArr)    // 先找数组中最大的峰值，该峰值左右两边递减
+	left := findTarget1(target, 0, mountainIndex, mountainArr)   // 先找峰值左边是否有target
+	if left != -1 {return left}
+	right := findTarget2(target, mountainIndex, length-1, mountainArr) // 再找峰值左边是否有target
+	if right != -1 {return right}
+	return -1
 }
 
-func findMountainLocation(target int, mountainArr *MountainArray) int {
+func findMountainLocation(mountainArr *MountainArray) int {   //寻找最大峰值
 	length := mountainArr.length()
 	h := 0
-	t := length-1
-	for h <= t {
+	t := length - 1
+	for h < t {
+		mid := h + (t-h)/2
+		if mountainArr.get(mid) > mountainArr.get(mid+1) {
+			t = mid
+		} else {
+			h = mid+1
+		}
+	}
+	return h
+}
+
+func findTarget1(target int, h int, t int, mountainArr *MountainArray) int {   // 从小到大
+	for h <=t {
 		mid := h + (t-h)/2
 		midValue := mountainArr.get(mid)
 		if midValue == target {return mid}
-		if
+		if midValue > target {t = mid-1}
+		if midValue < target {h = mid+1}
 	}
+	return -1
+}
+
+func findTarget2(target int, h int, t int, mountainArr *MountainArray) int {   // 从大到小
+	for h <=t {
+		mid := h + (t-h)/2
+		midValue := mountainArr.get(mid)
+		if midValue == target {return mid}
+		if midValue > target {h = mid+1}
+		if midValue < target {t = mid-1}
+	}
+	return -1
 }
---
+++ 牛客网/360/2020.4.30.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+func main() {
+	//a := 0
+	//fmt.Scan(&a)
+	//fmt.Printf("%d\n", a)
+	var n, m int
+	var str string
+	for {
+		_, err := fmt.Scan(&n, &m, &str)
+		if err ==io.EOF {break}
+		count := 0  // 满足两个.连在一起的数量，比如“..”为1，“...”为2
+		var word byte
+		for i:=0; i<n; i++ {
+			if word == '.' && str[i] == '.' {
+				count++
+			} else {
+				word = str[i]
+			}
+		}
+		var a int
+		var b uint8
+		arr := make([]byte, n)
+		for i:=0; i<n; i++ {
+			arr[i] = str[i]
+		}
+		for j:=0; j<m; j++ {
+			fmt.Scan(&a, &b)
+			if b == '.' {
+				if arr[a-1] == '.' {continue}
+				arr[a-1] = b
+				if a-2 >=0 && arr[a-2] == '.' {count++}
+				if a < n && arr[a] == '.' {count++}
+			}
+			if b != '.' {
+				if arr[a-1] == '.' {
+					if a-2 >=0 && arr[a-2] == '.' {count--}
+					if a < n && arr[a] == '.' {count--}
+				}
+				arr[a-1] = b
+			}
+			fmt.Println(count)
+		}
+	}
+}
