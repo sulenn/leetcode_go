c5addc961cf6832e26857a9ae345f5dd253d814e
sulenn <273409891@qq.com>
2020-05-07 09:40:39 +0800

    add and solve '另一个树的子树' algorithm

---
+++ leetcode/Dynamic Programing/983. 最低票价/mincostTickets_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_mincostTickets(T *testing.T) {
+	//fmt.Println(mincostTickets([]int{1,4,6,7,8,20}, []int{2,7,15}))
+	fmt.Println(mincostTickets([]int{1,2,3,4,5,6,7,8,9,10,30,31}, []int{2,7,15}))
+}
