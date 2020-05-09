8b6ce0e5d8081f05f45712c19038b002278be35d
sulenn <273409891@qq.com>
2020-05-09 09:49:26 +0800

    add and solve '69. x的平方根' algorithm

---
+++ leetcode/每日一题/69. x 的平方根/mySqrt.go
@@ -0,0 +1,18 @@
+package main
+
+//https://leetcode-cn.com/problems/sqrtx/
+
+func mySqrt(x int) int {
+	l := 1
+	r := x
+	for l <= r {
+		mid := l+(r-l)/2
+		if x / mid == mid {return mid}
+		if x / mid > mid {
+			l = mid+1
+		} else {
+			r = mid-1
+		}
+	}
+	return l-1
+}
---
+++ leetcode/每日一题/69. x 的平方根/mySqrt_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_mySqrt(T *testing.T) {
+	fmt.Println(mySqrt(4))
+	fmt.Println(mySqrt(8))
+	fmt.Println(mySqrt(9))
+	fmt.Println(mySqrt(11))
+	fmt.Println(mySqrt(1))
+	fmt.Println(mySqrt(0))
+	fmt.Println(mySqrt(20))
+}
