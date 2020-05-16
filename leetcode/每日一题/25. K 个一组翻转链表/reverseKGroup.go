dd37b61907b248695d94e1a21db8235b61348a96
sulenn <273409891@qq.com>
2020-05-16 09:33:47 +0800

    add and solve '25. K个一组翻转链表' algorithm

---
+++ leetcode/每日一题/25. K 个一组翻转链表/reverseKGroup.go
@@ -0,0 +1,48 @@
+package main
+
+//https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	p := &ListNode{0,nil}  // 指向头指针
+	t := p // 指向尾指针
+	curNode := head   // 用于计算链表剩余结点数，判断是否满足一轮 k
+	count := 0
+	for curNode != nil{
+		count = 0
+		for curNode != nil && count < k {  // 判断剩余节点数量是否满足 k
+			curNode = curNode.Next
+			count++
+		}
+		if count == k {
+			curTail, curHead, restHead := reverseByK(head, k)
+			t.Next = curHead
+			t = curTail
+			head = restHead
+		} else {  // 剩余节点数量不满足 k
+			t.Next = head
+		}
+	}
+	return p.Next
+}
+
+// 反转 k 个几点，返回反转后的链表和剩余的链表
+func reverseByK(head *ListNode, k int) (*ListNode, *ListNode, *ListNode) {  // 分别为分离出来的子链表尾、子链表头和剩余链表头
+	if head == nil {return nil,nil,nil}
+	first := head
+	second := first.Next
+	first.Next = nil
+	count := 1
+	for second != nil && count < k {
+		temp := second.Next
+		second.Next = first
+		first = second
+		second = temp
+		count++
+	}
+	return head, first, second
+}
---
+++ leetcode/每日一题/25. K 个一组翻转链表/reverseKGroup_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_reverseKGroup(T *testing.T) {
+	head := &ListNode{1,&ListNode{2,&ListNode{3,&ListNode{4, &ListNode{5,nil}}}}}
+	//head = reverseKGroup(head, 2)
+	head = reverseKGroup(head, 3)
+	print(head)
+}
+
+func print(head *ListNode)  {
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
+}
