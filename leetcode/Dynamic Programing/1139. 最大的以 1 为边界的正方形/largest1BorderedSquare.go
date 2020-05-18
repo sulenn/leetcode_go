fec350b44340b7a545e4240103895d53a8b3ddb0
sulenn <273409891@qq.com>
2020-05-18 09:31:52 +0800

    add and sove '1139. 最大的以1为边界的正方形' algorithm

---
+++ leetcode/Dynamic Programing/1139. 最大的以 1 为边界的正方形/largest1BorderedSquare.go
@@ -0,0 +1,36 @@
+package main
+
+//https://leetcode-cn.com/problems/largest-1-bordered-square/
+
+// 这个博主的很容易理解（解法不同）：https://leetcode-cn.com/problems/largest-1-bordered-square/solution/golang-20ms-jian-dan-yi-dong-by-resara/
+// 另外一个 dp 解法：https://leetcode-cn.com/problems/largest-1-bordered-square/solution/dpzui-jian-dan-de-dai-ma-by-ri-mu-tu-yuan-12/
+func largest1BorderedSquare(grid [][]int) int {
+	r := len(grid)
+	c := len(grid[0])
+	max := 0
+	for i:=0; i<r; i++ {
+		for j:=0; j<c; j++ {
+			if grid[i][j] != 1 {continue}
+			length := 1
+			for i+length < r && j+length < c {   // 获取当前点开始的最长边长，边长满足水平和垂直均为1
+				if grid[i+length][j] == 1 && grid[i][j+length] == 1 {
+					length++
+				} else {break}
+			}
+			length--  // 防止溢出
+			for length > 0 {
+				flag := true
+				for v:=1; v<=length; v++ {   // 满足正方型条件的最长边长
+					if grid[i+v][j+length] != 1 || grid[i+length][j+v] != 1 {
+						flag = false
+						break
+					}
+				}
+				if flag {break}
+				length--
+			}
+			if max < (length+1)*(length+1) {max = (length+1)*(length+1)}   // 更新最大正方形面积
+		}
+	}
+	return max
+}
---
+++ leetcode/Dynamic Programing/1139. 最大的以 1 为边界的正方形/largest1BorderedSquare_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_largest1BorderedSquare(T *testing.T) {
+	fmt.Println(largest1BorderedSquare([][]int{{1,1,1},{1,0,1},{1,1,1}}))
+}
