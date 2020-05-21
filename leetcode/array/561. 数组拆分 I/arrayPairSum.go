836480e01d91b632223d8c77c09d9b466f0dca69
sulenn <273409891@qq.com>
2020-05-21 08:15:57 +0800

    add and solve '561. 数组拆分 I' algorithm

---
+++ leetcode/array/561. 数组拆分 I/arrayPairSum.go
@@ -0,0 +1,14 @@
+package main
+
+import "sort"
+
+//https://leetcode-cn.com/problems/array-partition-i/
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i:=0; i<len(nums); i+=2 {
+		sum += nums[i]
+	}
+	return sum
+}
