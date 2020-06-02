8600958176ee4f86a8c94b16445b14c7eece80ca
sulenn <273409891@qq.com>
2020-06-02 08:07:26 +0800

    add and solve '面试题64. 求1+2+...+n' algorithm

---
+++ leetcode/每日一题/面试题64. 求1+2+…+n/sumNums.go
@@ -0,0 +1,16 @@
+package main
+
+//https://leetcode-cn.com/problems/qiu-12n-lcof/
+
+//参考（逻辑运算符的短路效应）：https://leetcode-cn.com/problems/qiu-12n-lcof/solution/mian-shi-ti-64-qiu-1-2-nluo-ji-fu-duan-lu-qing-xi-/
+func sumNums(n int) int {
+	res := 0
+	var recursive func(n int) int
+	recursive = func(n int) int {
+		_ = n>1 && recursive(n-1) > 0
+		res += n
+		return res
+	}
+	recursive(n)
+	return res
+}
---
+++ leetcode/每日一题/面试题64. 求1+2+…+n/sumNums_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_sumNums(T *testing.T) {
+	fmt.Println(sumNums(3))
+	fmt.Println(sumNums(9))
+}
