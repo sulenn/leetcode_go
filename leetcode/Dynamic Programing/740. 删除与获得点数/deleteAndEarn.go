7a05f9787432b1fcd9588352c1f88ad226200746
sulenn <273409891@qq.com>
2020-06-19 09:28:17 +0800

    add and solve '740. 删除与获得点数' algorithm

---
+++ leetcode/Dynamic Programing/740. 删除与获得点数/deleteAndEarn.go
@@ -0,0 +1,19 @@
+package main
+
+//dp[i] = max(dp[i-1], dp[i-2]+sort[i]*i)
+func deleteAndEarn(nums []int) int {
+	sort := make([]int, 10001)
+	for i:=0; i<len(nums); i++ {
+		sort[nums[i]]++
+	}
+	dp := make([]int, 10001)
+	dp[1] = sort[1]*1
+	for i:=2; i<=10000; i++ {
+		if dp[i-1] > dp[i-2]+sort[i]*i {
+			dp[i] = dp[i-1]
+		} else {
+			dp[i] = dp[i-2]+sort[i]*i
+		}
+	}
+	return dp[10000]
+}
---
+++ leetcode/Dynamic Programing/740. 删除与获得点数/deleteAndEarn_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_deleteAndEarn(T *testing.T)  {
+	fmt.Println(deleteAndEarn([]int{3, 4, 2}))
+}
