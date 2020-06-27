aacd731f966f3e39b5d119768e03ee3492b50975
sulenn <273409891@qq.com>
2020-06-27 11:24:58 +0800

    add and solve '16. 最接近的三数之和' algorithm

---
+++ leetcode/每日一题/16. 最接近的三数之和/threeSumClosest.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"sort"
+)
+
+//https://leetcode-cn.com/problems/3sum-closest/
+
+// 思路：排序数组。固定一个值，然后双指针遍历
+func threeSumClosest(nums []int, target int) int {
+	sum := 99999
+	sort.Ints(nums)
+	for i:=0; i<len(nums)-2; i++ {
+		if nums[i] > target {
+
+		}
+		pre := i+1
+		tail := len(nums)-1
+		for pre < tail {
+			res := nums[i] + nums[pre] + nums[tail] - target
+			if res == 0 {
+				return target
+			} else if res > 0 {
+				if abs(sum - target) > res {
+					sum = nums[i] + nums[pre] + nums[tail]
+				}
+				tail--
+			} else {
+				if abs(sum - target) > abs(res) {
+					sum = nums[i] + nums[pre] + nums[tail]
+				}
+				pre++
+			}
+		}
+	}
+	return sum
+}
+
+func abs(num int) int {
+	if num < 0 {
+		return -num
+	}
+	return num
+}
