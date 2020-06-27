8083d2e66426a84604e49984ceebe3f8129be9a4
sulenn <273409891@qq.com>
2020-06-27 10:17:12 +0800

    add and solve '41. 缺失的第一个正数' algorithm

---
+++ leetcode/每日一题/41. 缺失的第一个正数/firstMissingPositive.go
@@ -0,0 +1,25 @@
+package main
+
+//https://leetcode-cn.com/problems/first-missing-positive/
+
+// 思路：原地按下标排序，利用数据本身空间
+func firstMissingPositive(nums []int) int {
+	length := len(nums)
+	for i:=0; i<length; { // 原地桶排序
+		if nums[i]-1 > i {
+			if nums[i]-1 < length && nums[nums[i]-1] != nums[i] {
+				nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+				continue
+			}
+		} else if nums[i] > 0 {
+			nums[nums[i]-1] = nums[i]
+		}
+		i++
+	}
+	for i:=0; i<length; i++ {  // 查找缺失的最小正整数
+		if nums[i] != i+1 {
+			return i+1
+		}
+	}
+	return length+1
+}
---
+++ leetcode/每日一题/41. 缺失的第一个正数/firstMissingPositive_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_firstMissingPositive(T *testing.T)  {
+	fmt.Println(firstMissingPositive([]int {1,2,0}))
+	fmt.Println(firstMissingPositive([]int {3,4,-1,1}))
+	fmt.Println(firstMissingPositive([]int {7,8,9,11,12}))
+}
