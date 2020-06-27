d26968740247253abdd90d3371343d0ce173dee6
sulenn <273409891@qq.com>
2020-06-27 17:19:27 +0800

    supplement

--- leetcode/Dynamic Programing/面试题 16.18. 模式匹配/patternMatching.go
+++ leetcode/Dynamic Programing/面试题 16.18. 模式匹配/patternMatching.go
@@ -2,39 +2,94 @@ package main
 
 //https://leetcode-cn.com/problems/pattern-matching-lcci/
 
+//抄袭：https://leetcode-cn.com/problems/pattern-matching-lcci/solution/shuang-bai-golangti-jie-dai-ying-wen-zhu-shi-by-uk/
 func patternMatching(pattern string, value string) bool {
-	for i:=0; i<=len(value); i++ {
-		for j:=i+1; j<=len(value); j++ {
-			if judgement(value[:i], value[i:j], pattern, value) {
+	countA, countB := 0, 0
+	for _, v := range pattern {
+		if v == 'a' {
+			countA++
+		}
+		// we don't do countB++ here to save the INC cycles when len(pattern) is quite long
+	}
+	countB = len(pattern) - countA
+
+	if value == "" {
+		// either no 'a' or no 'b' can lead to this result
+		return countA == 0 || countB == 0
+	}
+	if pattern == "" {
+		// value has to be "" otherwise it violates previous if statement
+		return value == ""
+	}
+
+	lenV := len(value)
+
+	// given: len(value) > 0 AND
+	//   len(value) == countA*lenA + countB*lenB, find lenA and lenB in type of integer
+	if countA == 0 {
+		// countB can't be 0
+		if lenV%countB == 0 {
+			return match(value, pattern, 0, lenV/countB)
+		} else {
+			return false
+		}
+	}
+	if countB == 0 {
+		// countA can't be 0
+		if lenV%countA == 0 {
+			return match(value, pattern, lenV/countA, 0)
+		} else {
+			return false
+		}
+	}
+	hopeB := 0
+	// countA and countB both are non-zero, find lenA and lenB
+	for lenA := 0; lenA*countA <= lenV; lenA++ {
+		hopeB = lenV - lenA*countA
+		if hopeB%countB == 0 {
+			if match(value, pattern, lenA, hopeB/countB) {
 				return true
 			}
+			// not answer, continue
 		}
+		// not answer, continue
 	}
 	return false
 }
 
-func judgement(a string, b string, pattern string, value string) bool {
-	j := 0   // value 索引下标
-	lenA := len(a)
-	lenB := len(b)
-	lenValue := len(value)
-	for i:=0; i<len(pattern); i++ {
-		if pattern[i] == 'a' && j+lenA <= lenValue && a == value[j:j+lenA] {
-			j += lenA
+func match(value, pattern string, lenA, lenB int) bool {
+	str, strA, strB := "", "", ""
+	p := 0
+	// iterate every pattern character to validate the corresponding
+	// string slice from "value" matches the first appeared patterned string of this pattern.
+	// be careful lenA or lenB can be zero here.
+	for _, c := range pattern {
+		if c == 'a' {
+			if lenA == 0 {
+				continue
+			}
+			str = value[p : p+lenA]
+			if strA == "" {
+				strA = str
+			}
+			if strA != str {
+				return false
+			}
+			p += lenA
 			continue
 		}
-		if pattern[i] == 'b' && j+lenB <= lenValue &&  b == value[j:j+lenB] {
-			j += lenB
+		// else 'b'
+		if lenB == 0 {
 			continue
 		}
-		return false
-	}
-	if j != lenValue {
-		return false
+		str = value[p : p+lenB]
+		if strB == "" {
+			strB = str
+		}
+		if strB != str {
+			return false
+		}
+		p += lenB
 	}
 	return true
 }
-
-//func backtracking()  {
-//
-//}
--- leetcode/Dynamic Programing/面试题 16.18. 模式匹配/patternMatching_test.go
+++ leetcode/Dynamic Programing/面试题 16.18. 模式匹配/patternMatching_test.go
@@ -6,8 +6,9 @@ import (
 )
 
 func Test_patternMatching(t *testing.T)  {
-	fmt.Println(patternMatching("abba", "dogcatcatdog"))
-	fmt.Println(patternMatching("abba", "dogcatcatfish"))
-	fmt.Println(patternMatching("aaaa", "dogcatcatdog"))
-	fmt.Println(patternMatching("abba", "dogdogdogdog"))
+	//fmt.Println(patternMatching("abba", "dogcatcatdog"))
+	//fmt.Println(patternMatching("abba", "dogcatcatfish"))
+	//fmt.Println(patternMatching("aaaa", "dogcatcatdog"))
+	//fmt.Println(patternMatching("abba", "dogdogdogdog"))
+	fmt.Println(patternMatching("bbba", "xxxxxxy"))
 }
