99748ae0a39cdf075eb9ab2390767e38fda54c0c
sulenn <273409891@qq.com>
2020-06-28 09:25:14 +0800

    add and solve '209. 长度最小的子数组' algorithm

---
+++ leetcode/每日一题/209. 长度最小的子数组/minSubArrayLen.go
@@ -0,0 +1,35 @@
+package main
+
+import "math"
+
+//https://leetcode-cn.com/problems/minimum-size-subarray-sum/
+
+// 双指针
+func minSubArrayLen(s int, nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	rel := math.MaxInt64
+	first := 0
+	sum := 0
+	for i:=0; i<=len(nums); i++ {
+		for sum >= s {
+			if i - first < rel {
+				rel = i - first
+			}
+			if first == i-1 {
+				return rel
+			}
+			sum -= nums[first]
+			first++
+		}
+		if i == len(nums) {   // 数组中最后一个数，边界值溢出
+			break
+		}
+		sum += nums[i]
+	}
+	if rel == math.MaxInt64 {
+		return 0
+	}
+	return rel
+}
