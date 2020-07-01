e0b47ae9e61f404ba4eadf3571e9a04d5e1f9b9b
sulenn <273409891@qq.com>
2020-07-01 09:17:05 +0800

    add and solve '718. 最长重复子数组' algorithm

---
+++ leetcode/每日一题/718. 最长重复子数组/findLength.go
@@ -0,0 +1,30 @@
+package main
+
+//https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray/
+
+//思路：暴力，依次遍历所有
+func findLength(A []int, B []int) int {
+	max := 0
+	for i:=0; i<len(A); i++ {
+		for j:=0; j<len(B); j++ {
+			if A[i] == B[j] {
+				num_i := i+1
+				num_j := j+1
+				for num_i<len(A) && num_j<len(B) && A[num_i] == B[num_j] {
+					num_i++
+					num_j++
+				}
+				if num_i - i > max {  // 最大值比较
+					max = num_i - i
+				}
+			}
+			if len(B)-j-1 <= max {   // 剪枝
+				break
+			}
+		}
+		if len(A)-i-1 <= max {   // 剪枝
+			break
+		}
+	}
+	return max
+}
