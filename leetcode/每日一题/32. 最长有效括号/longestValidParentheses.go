e1c2327432147cb3217ef852b203442fb0fb5881
sulenn <273409891@qq.com>
2020-07-05 16:48:36 +0800

    add and solve '32. 最长有效括号' algorithm

---
+++ leetcode/每日一题/32. 最长有效括号/longestValidParentheses.go
@@ -0,0 +1,25 @@
+package main
+
+//https://leetcode-cn.com/problems/longest-valid-parentheses/
+
+//参考：https://leetcode-cn.com/problems/longest-valid-parentheses/solution/shou-hua-tu-jie-zhan-de-xiang-xi-si-lu-by-hyj8/
+
+func longestValidParentheses(s string) int {
+	stack := []int {-1}   // 初始值
+	max := 0
+	for i:=0; i<len(s); i++ {
+		if s[i] == '(' {
+			stack = append(stack, i)
+			continue
+		}
+		if s[i] == ')' && len(stack) > 1 {
+			if i - stack[len(stack)-2] > max {
+				max = i - stack[len(stack)-2]
+			}
+			stack = stack[:len(stack)-1]
+		} else if s[i] == ')' {
+			stack = []int {i}
+		}
+	}
+	return max
+}
---
+++ leetcode/每日一题/32. 最长有效括号/longestValidParentheses_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_longestValidParentheses(T *testing.T)  {
+	fmt.Println(longestValidParentheses(")()())"))
+}
