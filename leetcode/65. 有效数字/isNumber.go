539e52f42e671a12a59241e1da4f2cd27b7d56f8
sulenn <273409891@qq.com>
2020-08-03 09:26:07 +0800

    add and solve '65. 有效数字 '  algorithm

--- leetcode/65. 有效数字/isNumber.go
+++ leetcode/65. 有效数字/isNumber.go
@@ -4,6 +4,8 @@ import "fmt"
 
 //https://leetcode-cn.com/problems/valid-number/
 
+// 参考《剑指 offer》
+
 func isNumber(s string) bool {
 	for len(s) != 0 && s[0] == ' ' { // 过滤前导空格
 		s = s[1:]
@@ -11,106 +13,52 @@ func isNumber(s string) bool {
 	for len(s) != 0 && s[len(s)-1] == ' ' { // 过滤后导空格
 		s = s[:len(s)-1]
 	}
-	if len(s) == 0 {
-		return false
-	}
-	length := len(s)
 	index := 0
-	if !scanA(s, length, &index) { // 满足 A
-		return false
-	}
-	if index == length {
-		return true
-	}
-	if length == 1 && s == "." {
-		return false
-	}
-	if s[index] == '.' { // 满足 B
+	flag := judgeInteger(s, &index)
+	if index < len(s) && s[index] == '.' {
 		index++
-		if !scanB(s, length, &index) {
-			return false
-		}
+		flag = judgeUnsignedInteger(s, &index) || flag
 	}
-	if index == length {
-		return true
-	}
-
-	if index != 0 && (s[index] == 'e' || s[index] == 'E') { // 满足 C
+	if index < len(s) && (s[index] == 'e' || s[index] == 'E') {
 		index++
-		if !scanC(s, length, &index) {
-			return false
-		}
+		flag = judgeInteger(s, &index) && flag
 	}
-	return index == length
+	return flag && index == len(s)
 }
 
-func scanA(s string, length int, index *int) bool {
-	for *index < length {
-		if (s[*index] == '+' || s[*index] == '-') && *index != 0 {
-			return false
-		}
-		if s[*index] == '.' {
-			return true
-		}
-		if s[*index] == 'e' || s[*index] == 'E' {
-			return true
-		}
-		if s[*index] < '0' || s[*index] > '9' {
-			return false
-		}
+func judgeInteger(s string, index *int) bool {
+	if *index < len(s) && (s[*index] == '+' || s[*index] == '-') {
 		*index++
 	}
-	return true
+	return judgeUnsignedInteger(s, index)
 }
 
-func scanB(s string, length int, index *int) bool {
-	for *index < length {
-		if s[*index] == 'e' || s[*index] == 'E' {
-			return true
-		}
-		if s[*index] < '0' || s[*index] > '9' {
-			return false
-		}
+func judgeUnsignedInteger(s string, index *int) bool {
+	initIndex := *index
+	for *index < len(s) && s[*index] >= '0' && s[*index] <= '9' {
 		*index++
 	}
-	return true
-}
-
-func scanC(s string, length int, index *int) bool {
-	if length == *index {
-		return false
-	}
-	initIndex := index
-	for *index < length {
-		if (s[*index] == '+' || s[*index] == '-') && index == initIndex {
-			*index++
-			continue
-		}
-		if s[*index] < '0' || s[*index] > '9' {
-			return false
-		}
-		*index++
-	}
-	return true
+	return *index > initIndex
 }
 
 func main() {
-	//fmt.Println(isNumber("0"))
-	//fmt.Println(isNumber(" 0.1"))
-	//fmt.Println(isNumber("abc"))
-	//fmt.Println(isNumber("1 a"))
-	//fmt.Println(isNumber("2e10"))
-	//fmt.Println(isNumber(" -90e3"))
-	//fmt.Println(isNumber(" 1e"))
-	//fmt.Println(isNumber("e3"))
-	//fmt.Println(isNumber(" 6e-1"))
-	//fmt.Println(isNumber(" 99e2.5 "))
-	//fmt.Println(isNumber("53.5e93"))
-	//fmt.Println(isNumber(" --6"))
-	//fmt.Println(isNumber("-+3"))
-	//fmt.Println(isNumber("95a54e53"))
+	fmt.Println(isNumber("0"))
+	fmt.Println(isNumber(" 0.1"))
+	fmt.Println(isNumber("abc"))
+	fmt.Println(isNumber("1 a"))
+	fmt.Println(isNumber("2e10"))
+	fmt.Println(isNumber(" -90e3"))
+	fmt.Println(isNumber(" 1e"))
+	fmt.Println(isNumber("e3"))
+	fmt.Println(isNumber(" 6e-1"))
+	fmt.Println(isNumber(" 99e2.5 "))
+	fmt.Println(isNumber("53.5e93"))
+	fmt.Println(isNumber(" --6"))
+	fmt.Println(isNumber("-+3"))
+	fmt.Println(isNumber("95a54e53"))
 	fmt.Println(isNumber("1 "))
 	fmt.Println(isNumber(".1"))
 	fmt.Println(isNumber("3."))
 	fmt.Println(isNumber("."))
+	fmt.Println(isNumber(""))
 }
