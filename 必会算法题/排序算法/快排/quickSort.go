aab67e4d56c54e558950e0c3a2b9ca32209e04de
sulenn <273409891@qq.com>
2020-09-01 09:16:25 +0800

    perf: perfect quick sort

--- 必会算法题/排序算法/快排/quickSort.go
+++ 必会算法题/排序算法/快排/quickSort.go
@@ -25,3 +25,29 @@ func sort(nums []int) {
 	sort(nums[:fixed])
 	sort(nums[fixed+1:])
 }
+
+func quickSort(nums []int) { // 真正的快排
+	if len(nums) == 0 {
+		return
+	}
+	l := 0
+	r := len(nums) - 1
+	partition(nums, l, r)
+}
+
+func partition(nums []int, l, r int) { // 快排精髓
+	if l >= r {
+		return
+	}
+	v := nums[l]
+	less := l
+	for i := l + 1; i <= r; i++ {
+		if nums[i] < v {
+			less++
+			nums[less], nums[i] = nums[i], nums[less]
+		}
+	}
+	nums[l], nums[less] = nums[less], nums[l]
+	partition(nums, l, less-1)
+	partition(nums, less+1, r)
+}
--- 必会算法题/排序算法/快排/quickSort_test.go
+++ 必会算法题/排序算法/快排/quickSort_test.go
@@ -9,4 +9,7 @@ func Test_quickSort(T *testing.T) {
 	test1 := []int{2, 1, 5, 4, 7, 6, 8, 0}
 	sort(test1)
 	fmt.Println(test1)
+	test2 := []int{2, 1, 6, 3, 76, 8, 444, 87, 93, 0}
+	quickSort(test2)
+	fmt.Println(test2)
 }
