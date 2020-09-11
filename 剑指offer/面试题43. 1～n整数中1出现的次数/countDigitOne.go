902f32fceac011cc2aae31d13e85e83b0223a137
sulenn <273409891@qq.com>
2020-09-11 20:47:57 +0800

    perf: modify '1~n 整数中1出现的次数' algorithm

--- 剑指offer/面试题43. 1～n整数中1出现的次数/countDigitOne.go
+++ 剑指offer/面试题43. 1～n整数中1出现的次数/countDigitOne.go
@@ -1,26 +1,38 @@
 package main
 
+import "fmt"
+
 //https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/
 
-//参考：https://leetcode-cn.com/problems/number-of-digit-one/solution/shu-zi-1-de-ge-shu-by-leetcode/
-//每一位上1的数量公式：countr += (n / divider) * i + min(max(n % divider - i + 1, 0), i);
+//参考：https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/solution/mian-shi-ti-43-1n-zheng-shu-zhong-1-chu-xian-de-2/
+//1. cur=0, high×digit
+//2. cur=1, high×digit+low+1
+//3. cur>1, (high+1)×digit
 func countDigitOne(n int) int {
+	digit := 1
+	high := n / 10
+	low := 0
+	cur := n % 10
 	result := 0
-	for i := 1; i <= n; i *= 10 {
-		divider := i * 10
-		temp1 := 0
-		temp2 := 0
-		if n%divider-i+1 > 0 {
-			temp1 = n%divider - i + 1
-		} else {
-			temp1 = 0
+	for high != 0 || cur != 0 {
+		if cur == 0 {
+			result += high * digit
 		}
-		if temp1 < i {
-			temp2 = temp1
-		} else {
-			temp2 = i
+		if cur == 1 {
+			result += high*digit + low + 1
 		}
-		result += n/divider*i + temp2
+		if cur > 1 {
+			result += (high + 1) * digit
+		}
+		digit *= 10
+		cur = high % 10
+		high /= 10
+		low = n % digit
 	}
 	return result
+}
+
+func main() {
+	fmt.Println(countDigitOne(12))
+	fmt.Println(countDigitOne(13))
 }
