397599b1efa813faa948ce595d934c352a13742d
sulenn <273409891@qq.com>
2020-09-28 08:36:54 +0800

    feat: add and solve '116. 填充每个节点的下一个右侧节点指针' algorithm

---
+++ leetcode/116. 填充每个节点的下一个右侧节点指针/main.go
@@ -0,0 +1,25 @@
+package main
+
+//https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	if root.Left != nil {
+		root.Left.Next = root.Right
+	}
+	if root.Right != nil && root.Next != nil {
+		root.Right.Next = root.Next.Left
+	}
+	connect(root.Left)
+	connect(root.Right)
+	return root
+}
