9213e966d56cf4d972c8694e08bb53922b180b6c
sulenn <273409891@qq.com>
2020-09-30 09:49:48 +0800

    feat: add and solve '700. 二叉搜索树中的搜索' algorithm

---
+++ leetcode/700. 二叉搜索树中的搜索 /main.go
@@ -0,0 +1,23 @@
+package main
+
+//https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Val == val {
+		return root
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	} else {
+		return searchBST(root.Right, val)
+	}
+}
