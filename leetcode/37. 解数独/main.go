cf62d170118bbdca2ab809e6fff4e8aed8eb2ea8
sulenn <273409891@qq.com>
2020-10-03 17:00:44 +0800

    feat: add and solve '37. 解数独' algorithm

--- leetcode/37. 解数独/main.go
+++ leetcode/37. 解数独/main.go
@@ -2,24 +2,44 @@ package main
 
 //https://leetcode-cn.com/problems/sudoku-solver/
 
+//参考：https://leetcode-cn.com/problems/sudoku-solver/solution/di-gui-hui-su-by-f3klu7mkmi-2/
 func solveSudoku(board [][]byte) {
-	lineArrs := make([][]int, 0)
-	columnArrs := make([][]int, 0)
-
-	length := len(board)
-	width := len(board[0])
-	for i := 0; i < length; i++ {
-		for j := 0; j < width; j++ {
-
+	row := [9][9]bool{}
+	col := [9][9]bool{}
+	box := [9][9]bool{}
+	for rowK := 0; rowK < 9; rowK++ {
+		for colK := 0; colK < 9; colK++ {
+			if board[rowK][colK] != '.' {
+				num := board[rowK][colK] - '1'
+				boxK := (rowK/3)*3 + colK/3
+				row[rowK][num], col[colK][num], box[boxK][num] = true, true, true
+			}
 		}
 	}
+	fill(board, 0, row, col, box)
 }
 
-func fill(board [][]byte, x, y int) bool {
-	if board[x][y] != '.' {
-		if x < 8 {
-			return fill(board, x+1, y)
-		}
-
+func fill(board [][]byte, n int, row [9][9]bool, col [9][9]bool, box [9][9]bool) bool {
+	if n == 81 {
+		return true
 	}
+	rowK := n / 9
+	colK := n % 9
+	if board[rowK][colK] != '.' {
+		return fill(board, n+1, row, col, box)
+	}
+
+	boxK := (rowK/3)*3 + colK/3
+	for num := 0; num < 9; num++ {
+		if !row[rowK][num] && !col[colK][num] && !box[boxK][num] {
+			board[rowK][colK] = byte('1' + num)
+			row[rowK][num], col[colK][num], box[boxK][num] = true, true, true
+			if fill(board, n+1, row, col, box) { //下一个填充
+				return true
+			}
+			row[rowK][num], col[colK][num], box[boxK][num] = false, false, false //失败回溯
+		}
+	}
+	board[rowK][colK] = '.'
+	return false
 }
