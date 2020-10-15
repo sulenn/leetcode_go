00634cc785f523d9a1c919b0e82f01c05b2de78f
sulenn <273409891@qq.com>
2020-10-15 09:09:09 +0800

    feat: add and solve '1480. 一维数组的动态和' algorithm

--- leetcode/1404.将二进制表示减到 1 的步骤数/main.go
+++ leetcode/1404.将二进制表示减到 1 的步骤数/main.go
@@ -1,1 +1,27 @@
-package _404_将二进制表示减到_1_的步骤数
+package main
+
+//https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+
+func numSteps(s string) int {
+	count := 0
+	add := 0
+	for s[0] == '0' {
+		s = s[1:]
+	}
+	for i := len(s) - 1; i > 0; i-- {
+		if s[i] == '0' && add == 0 {
+			count += 1
+		} else if s[i] == '0' && add == 1 {
+			count += 2
+		} else if s[i] == '1' && add == 1 {
+			count += 1
+		} else if s[i] == '1' && add == 0 {
+			count += 2
+			add = 1
+		}
+	}
+	if add == 1 {
+		count += 1
+	}
+	return count
+}
--- leetcode/530.二叉搜索树的最小绝对差/main.go
+++ leetcode/530.二叉搜索树的最小绝对差/main.go
@@ -1,1 +1,38 @@
-package _30_二叉搜索树的最小绝对差
+package main
+
+import (
+	"math"
+)
+
+//https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/
+
+//参考：https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/solution/er-cha-sou-suo-shu-jie-dian-zui-xiao-ju-chi-by-lee/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var min, prev int
+
+func getMinimumDifference(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	min = math.MaxInt64
+	prev = math.MinInt64
+	dfs(root)
+	return min
+}
+
+func dfs(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	dfs(root.Left)
+	if prev != math.MinInt64 && root.Val-prev < min {
+		min = root.Val - prev
+	}
+	prev = root.Val
+	dfs(root.Right)
+}
