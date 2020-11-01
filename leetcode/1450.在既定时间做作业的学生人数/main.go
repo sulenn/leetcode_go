0723a7cbdec6ffb47823254d9834e824ab7d0918
sulenn <273409891@qq.com>
2020-11-01 15:47:44 +0800

    feat: add and solve '1450. 在既定时间做作业的学生人数' algorithm

---
+++ leetcode/1450.在既定时间做作业的学生人数/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func busyStudent(startTime []int, endTime []int, queryTime int) int {
+	count := 0
+	for i := 0; i < len(startTime); i++ {
+		if startTime[i] <= queryTime && endTime[i] >= queryTime {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(busyStudent([]int{1, 1, 1, 1}, []int{1, 3, 2, 4}, 7))
+}
