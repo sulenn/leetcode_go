b76433492c12e41b92d07570d3b9b44e02e860a8
sulenn <273409891@qq.com>
2021-02-06 22:50:46 +0800

    docs: update content

--- main.go
+++ main.go
@@ -1,59 +1,28 @@
 package main
 
 import (
-	"os"
-
-	log "github.com/sirupsen/logrus"
+	"fmt"
 )
 
-var logger = log.New()
-
-func init() {
-	// Log as JSON instead of the default ASCII formatter.
-	//log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stderr)
-
-	// Only log the warning severity or above.
-	log.SetLevel(log.InfoLevel)
-
-	//log.se
+func main() {
+	nums := []int{3, 2, 6, 7, 3, 5, 8, 3, 9}
+	quickSort(nums, 0, len(nums)-1)
+	fmt.Println(nums)
 }
 
-func main() {
-	//logger.Out = os.Stdout
-	//
-	//_, err := os.OpenFile("logrus.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	//if err == nil {
-	//	logger.Out = os.Stdout
-	//} else {
-	//	logger.Info("Failed to log to file, using default stderr")
-	//}
-
-	logger.WithFields(log.Fields{
-		"animal": "walrus",
-		"size":   10,
-	}).Info("A group of walrus emerges from the ocean")
-
-	logger.WithFields(log.Fields{
-		"omg":    true,
-		"number": 122,
-	}).Warn("The group's number increased tremendously!")
-
-	logger.WithFields(log.Fields{
-		"omg":    true,
-		"number": 100,
-	}).Fatal("The ice breaks!")
-
-	// A common pattern is to re-use fields between logging statements by re-using
-	// the logrus.Entry returned from WithFields()
-	contextLogger := log.WithFields(log.Fields{
-		"common": "this is a common field",
-		"other":  "I also should be logged always",
-	})
-
-	contextLogger.Info("I'll be logged with common and other field")
-	contextLogger.Info("Me too")
+func quickSort(nums []int, start, end int) {
+	if end >= len(nums) || start >= end {
+		return
+	}
+	fixValue, point := start, start
+	for point <= end {
+		if nums[point] > nums[start] {
+			fixValue++
+			nums[fixValue], nums[point] = nums[point], nums[fixValue]
+		}
+		point++
+	}
+	nums[start], nums[fixValue] = nums[fixValue], nums[start]
+	quickSort(nums, start, fixValue-1)
+	quickSort(nums, fixValue+1, end)
 }
